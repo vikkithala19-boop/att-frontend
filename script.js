6f33cac713e39934b4c5588cf2a1aaf37135ea6a
vikkithala19-boop <vikkithala19@gmail.com>
2026-03-14 18:39:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 async function getAttendance(){
 
-let id = document.getElementById("id").value
-let pass = document.getElementById("pass").value
+document.getElementById("result").innerHTML="Checking attendance..."
 
-let res = await fetch("https://att-backend-lzzv.onrender.com/attendance",{
+let id=document.getElementById("id").value
+let pass=document.getElementById("pass").value
+
+let res=await fetch("https://att-backend-lzzv.onrender.com/attendance",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
@@ -14,28 +16,38 @@ password:pass
 })
 })
 
-let text = await res.text()
+let data=await res.json()
 
-try{
+let attended=parseInt(data.attended)
+let total=parseInt(data.total)
 
-let data = JSON.parse(text)
+let percent=(attended/total)*100
 
-let attended = parseInt(data.attended)
-let total = parseInt(data.total)
+document.getElementById("progress").style.width=percent+"%"
 
-let percent = (attended/total)*100
-let bunk = Math.floor((attended/0.75)-total)
+if(percent>=75){
 
-document.getElementById("result").innerHTML =
+let bunk=Math.floor((attended/0.75)-total)
+
+document.getElementById("result").innerHTML=
 `
 Attendance: ${percent.toFixed(2)}% <br>
 You can bunk: ${bunk} classes
 `
 
-}catch(err){
+}else{
 
-document.getElementById("result").innerHTML =
-"Backend error or server waking up. Try again."
+let need=0
+
+while((attended+need)/(total+need)<0.75){
+need++
+}
+
+document.getElementById("result").innerHTML=
+`
+Attendance: ${percent.toFixed(2)}% <br>
+You must attend ${need} classes to reach 75%
+`
 
 }
 
