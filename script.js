c8179f11fbdd6b1dc73ee351fecfc65f8c806558
vikkithala19-boop <vikkithala19@gmail.com>
2026-03-14 18:30:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function getAttendance(){
 let id = document.getElementById("id").value
 let pass = document.getElementById("pass").value
 
-let res = await fetch("https://att-backend-lzzv.onrender.com/attendance",{
+let res = await fetch("https://att-backend-lzzv.onrender.com/",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
